d9451ecf5d20f566b74e517cf49ab15d46c295ec
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-04-28 09:51:24 +0000

    Add CommentList and CommentDetail generic views

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_hbsvpw.jpg'
+        upload_to='images/', default='../default_profile_qdjgyp'
     )
 
     class Meta:
@@ -19,8 +19,10 @@ class Profile(models.Model):
     def __str__(self):
         return f"{self.owner}'s profile"
 
+
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(owner=instance)
+
 
 post_save.connect(create_profile, sender=User)
--- proj5/urls.py
+++ proj5/urls.py
@@ -21,6 +21,8 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
     path('', include('posts.urls')),
+    path('', include('comments.urls')),
+
 ]
 
 
