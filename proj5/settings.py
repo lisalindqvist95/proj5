63053bf0d9a07e5f20a9742b95a8fd90fab2a677
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-05-03 14:56:22 +0000

    Try to find bug with API connection

--- proj5/settings.py
+++ proj5/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOST'), 'localhost', ]
+ALLOWED_HOST = [os.environ.get('ALLOWED_HOST'), 'localhost', ]
 
 
 # Application definition
