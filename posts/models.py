a442d37e533798fd13b82b18ecdf14465088c45f
lisalindqvist95 <lindqvist.lisa@hotmail.com>
2023-06-29 06:30:37 +0000

    Remove new field from posts

--- posts/models.py
+++ posts/models.py
@@ -31,7 +31,6 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    makeup_products = models.TextField(blank=True)
     image = models.ImageField(
         upload_to='images/', default='../default_post_nhqkkf', blank=True
     )
--- posts/tests.py
+++ posts/tests.py
@@ -2,16 +2,3 @@ from django.contrib.auth.models import User
 from .models import Post
 from rest_framework import status
 from rest_framework.test import APITestCase
-
-
-class PostListViewTests(APITestCase):
-    def setUp(self):
-        User.objects.create_user(username='admin', password='admin123')
-
-    def test_can_list_posts(self):
-        adam = User.objects.get(username='admin')
-        Post.objects.create(owner=adam, title='a title')
-        response = self.client.get('/posts/')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        print(response.data)
-        print(len(response.data))
